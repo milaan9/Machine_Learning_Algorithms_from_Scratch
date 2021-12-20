c700b35c964e405fb9af8512f05d714731d3f887
Milaan Parmar /  Милан пармар / _米兰 帕尔马 <milaanparmar9@hotmail.com>
2021-12-20 16:16:43 +0800

    Delete 01_MATLAB directory

--- 01_MATLAB/K-Nearest Neigbour/code/normalise.m
+++
@@ -1,9 +0,0 @@
-function [data] = normalise(data, std, mean)
-    %disp(data);
-    for i = 1: (size(data, 2))
-        data(:, i) = data(:, i) - mean(1, i);
-
-        data(:, i) = data(:, i) / std(1, i) ;
-    end
-    %disp(data);
-end
--- 01_MATLAB/Singular Value Decomposition/README.txt
+++
@@ -1,7 +0,0 @@
-
-IN command window type : main('input1.txt', 2, 10)
-
-Here :
-	M = 2
-	iterations = 10
-
